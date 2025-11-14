1acea79bbe0b7d79aa1c275a423ce69795bfdcb1
kenkoro <frogharvard@gmail.com>
2025-11-14 20:31:17 +0700

    chore: initial setup

---
+++ docs/README.md
@@ -0,0 +1,3 @@
+[![Made with Supabase](https://supabase.com/badge-made-with-supabase-dark.svg)](https://supabase.com)
+
+A starter pack to explore the Android SDK.
---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven { url = uri("https://www.jitpack.io") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://www.jitpack.io") }
+    }
+}
+
+rootProject.name = "friendly-umbrella"
+
+include(":app")
